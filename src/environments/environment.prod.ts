e71bcb937845cffbcad8a2777d6144d453cb42d2
Dmitry <roarlove1337@gmail.com>
2020-06-21 20:32:46 +0700

    fix environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import {Environment} from './interface'
 export const environment: Environment = {
   production: true,
   apiKey: 'AIzaSyAf0Yl87DAJd0Z0xUtbM1KJlyxgs5jothI',
-  fbDbUrl: 'https://angular-udemy-practicum.firebaseio.com'
+  fbDbUrl: 'https://angular-blog-fdb13.firebaseio.com'
 };
